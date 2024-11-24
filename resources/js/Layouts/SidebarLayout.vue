c2a55537b682cceafce719f4aceedb4b65222a60
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-11-24 03:03:45 +0100

    toggle sidebar works

--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ChevronsLeftIcon, ChevronsRightIcon } from 'lucide-vue-next';
+import { Button } from 'primevue';
 import { ref } from 'vue';
 
 const isSidebarOpen = ref<boolean>(true);
@@ -9,33 +11,39 @@ const toggleSidebar = () => {
 
 <!-- TODO: Make sidebar responsive -->
 <template>
-    <div class="flex h-screen">
+    <div :class="`flex ${isSidebarOpen ? 'h-screen' : ''}`">
         <!-- Sidebar -->
         <div
-            :class="`bg-zinc-50 border-zinc-100 border-r transition-all duration-300 ${
+            :class="`bg-zinc-50 border-zinc-100 border-r transition-all duration-300 h-full overflow-hidden relative ${
                 isSidebarOpen ? 'w-64' : 'w-0'
             }`"
-            class="h-full overflow-hidden relative"
         >
             <div v-if="isSidebarOpen" class="p-5">
-                <!-- Close Button -->
-                <button
-                    @click="toggleSidebar"
-                    class="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full shadow-md hover:bg-red-600 transition">
-                    ✖
-                </button>
-                Menu comes here...
+                <div class="flex items-center justify-between mb-8">
+                    <p class="text-md font-semibold">nicefeature</p>
+                    <Button
+                        @click="toggleSidebar"
+                        severity="secondary"
+                        class="p-1"
+                    >
+                        <ChevronsLeftIcon :size="16" />
+                    </Button>
+                </div>
+                <div>
+                    Menu comes here...
+                </div>
             </div>
         </div>
 
         <!-- Open Button -->
-        <button
+        <Button
             v-if="!isSidebarOpen"
             @click="toggleSidebar"
-            class="absolute top-2 left-2 bg-blue-500 text-white p-2 rounded-full shadow-md hover:bg-blue-600 transition z-10"
+            severity="secondary"
+            class="p-1 m-4"
         >
-            ☰
-        </button>
+            <ChevronsRightIcon :size="16" />
+        </Button>
 
         <!-- Page Content -->
         <main class="flex-1">
