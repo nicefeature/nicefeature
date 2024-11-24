45df2ade265642796f98b9d20628f585ab4be5a1
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-11-24 02:30:15 +0100

    add sidebar

--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
-import { PlusIcon } from 'lucide-vue-next';
-import { Button } from 'primevue';
+import { ref } from 'vue';
 
+const isSidebarOpen = ref<boolean>(true);
+const toggleSidebar = () => {
+    isSidebarOpen.value = !isSidebarOpen.value;
+}
 </script>
 
+<!-- TODO: Make sidebar responsive -->
 <template>
-    <!-- Sidebar -->
-    Sidebar comes here...
+    <div class="flex h-screen">
+        <!-- Sidebar -->
+        <div
+            :class="`bg-zinc-50 border-zinc-100 border-r transition-all duration-300 ${
+                isSidebarOpen ? 'w-64' : 'w-0'
+            }`"
+            class="h-full overflow-hidden"
+        >
+            <div class="p-5">
+                Menu comes here...
+            </div>
+        </div>
 
-    <!-- Page Content -->
-    <main>
-        <slot />
-    </main>
+        <!-- Page Content -->
+        <main class="flex-1">
+            <slot />
+        </main>
+    </div>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,6 +7,6 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <SidebarLayout>
-        <div class="p-5">Dashboard comes here...</div>
+        <div class="px-10 py-5">Dashboard comes here...</div>
     </SidebarLayout>
 </template>
