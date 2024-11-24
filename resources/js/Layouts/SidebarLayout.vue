70d21cc0d2e79c4b44c8b79fecdabc274d88d7b6
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-11-24 01:57:31 +0100

    install primevue, configure with tailwind

--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import { PlusIcon } from 'lucide-vue-next';
+import { Button } from 'primevue';
+
 </script>
 
 <template>
-    Sidebar comes here...
-    <slot />
+    <div class="p-10">
+        Sidebar comes here... <br>
+        <Button class="text-sm font-semibold">
+            <PlusIcon :size="14" />
+            Create new board
+        </Button>
+        <slot />
+    </div>
 </template>
