e8469e99202d5342751ddb178d803136e08df51c
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-11-29 23:56:13 +0100

    create controllers and views

---
+++ resources/js/Pages/Feedback/Board/AdminBoard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AdminSidebarLayout from '@/Layouts/Sidebar/AdminSidebarLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<!-- Board title should come from DB, if it's null we put a placeholder -->
+<template>
+    <Head title="New Board" />
+
+    <AdminSidebarLayout>
+        <template #header>Admin » Feedback » New Board</template>
+        <h1 class="text-2xl font-semibold mb-2">New Board</h1>
+        <p class="text-primary-500">Description comes here...</p>
+    </AdminSidebarLayout>
+</template>
---
+++ resources/js/Pages/Feedback/Board/PublicBoard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PublicSidebarLayout from '@/Layouts/Sidebar/PublicSidebarLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<!-- Board title should come from DB, if it's null we put a placeholder -->
+<template>
+    <Head title="New Board" />
+
+    <PublicSidebarLayout>
+        <h1 class="text-2xl font-semibold mb-2">New Board</h1>
+        <p class="text-primary-500">Description comes here...</p>
+    </PublicSidebarLayout>
+</template>
