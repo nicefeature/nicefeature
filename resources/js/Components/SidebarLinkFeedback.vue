eb239f13ab042f15d7ecab0464e1ebd48e0566c4
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-12-01 01:53:00 +0100

    show board links

--- resources/js/Components/SidebarLinkFeedback.vue
+++ resources/js/Components/SidebarLinkFeedback.vue
@@ -2,7 +2,7 @@
 import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
 import { Button } from 'primevue';
-import { PlusIcon, StarIcon } from 'lucide-vue-next';
+import { DotIcon, LayersIcon, PlusIcon, SquareDashedIcon, StarIcon } from 'lucide-vue-next';
 
 const props = defineProps<{
     href: string;
@@ -10,10 +10,13 @@ const props = defineProps<{
     isAdminPage: boolean;
 }>();
 
+const activeClasses = 'bg-zinc-200 border-zinc-200 hover:bg-zinc-200 hover:border-zinc-200'
+const inactiveClasses = 'bg-zinc-50 border-zinc-50 hover:bg-zinc-200 hover:border-zinc-200'
+
 const classes = computed(() =>
     props.active
-        ? 'bg-zinc-200 border-zinc-200 hover:bg-zinc-200 hover:border-zinc-200'
-        : 'bg-zinc-50 border-zinc-50 hover:bg-zinc-200 hover:border-zinc-200',
+        ? activeClasses
+        : inactiveClasses,
 );
 </script>
 
@@ -40,20 +43,27 @@ const classes = computed(() =>
         v-if="isAdminPage && $page.props.boards.length === 0"
         class="text-xs text-primary-500 mb-4 mt-2 ml-4 mr-2"
     >
-        You have no feedback boards yet, but you can <Link class="underline cursor-pointer hover:text-primary-700" :href="route('admin.board.store')" method="post" as="link">create a new one</Link>
+        You have no feedback boards yet, but you can <Link class="underline cursor-pointer hover:text-primary-700" :href="route('admin.board.store')" method="post" as="button">create a new one</Link>
     </div>
     <div
         v-if="$page.props.boards.length > 0"
-        class="flex flex-col gap-1"
+        class="flex flex-col"
     >
         <Link
             v-for="board in $page.props.boards"
             as="button"
             :key="board.id"
             :href="isAdminPage ? route('admin.board.show', board.id) : route('board.show', board.id)"
-            class="text-left text-xs"
         >
-            {{ board.title || 'Untitled Board' }}
+            <Button
+                severity="secondary"
+                :class="route().current('admin.board.show', board.id) || route().current('board.show', board.id) ? activeClasses : inactiveClasses"
+                class="text-xs font-semibold py-1 pl-5 pr-2 w-full justify-between">
+                <div class="flex gap-2 items-end">
+                    <SquareDashedIcon :size="16" />
+                    {{ board.title || 'Untitled Board' }}
+                </div>
+            </Button>
         </Link>
     </div>
 </template>
