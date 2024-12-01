949f291363ba8467427f6e604cf99f3941597308
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-12-02 00:55:31 +0100

    show title

--- resources/js/Pages/Feedback/Board/AdminBoard.vue
+++ resources/js/Pages/Feedback/Board/AdminBoard.vue
@@ -5,11 +5,11 @@ import { Head } from '@inertiajs/vue3';
 
 <!-- Board title should come from DB, if it's null we put a placeholder -->
 <template>
-    <Head title="Untitled Board" />
+    <Head :title="$page.props.board.title || 'Untitled Board'" />
 
     <AdminSidebarLayout>
-        <template #header>Admin » Feedback » Untitled Board</template>
-        <h1 class="text-2xl font-semibold mb-2">Untitled Board</h1>
+        <template #header>Admin » Feedback » {{ $page.props.board.title || 'Untitled Board' }}</template>
+        <h1 class="text-2xl font-semibold mb-2">{{ $page.props.board.title || 'Untitled Board' }}</h1>
         <p class="text-primary-500">Description for {{ $page.props.board.id }} comes here...</p>
     </AdminSidebarLayout>
 </template>
--- resources/js/Pages/Feedback/Board/PublicBoard.vue
+++ resources/js/Pages/Feedback/Board/PublicBoard.vue
@@ -5,10 +5,10 @@ import { Head } from '@inertiajs/vue3';
 
 <!-- Board title should come from DB, if it's null we put a placeholder -->
 <template>
-    <Head title="Untitled Board" />
+    <Head :title="$page.props.board.title || 'Untitled Board'" />
 
     <PublicSidebarLayout>
-        <h1 class="text-2xl font-semibold mb-2">Untitled Board</h1>
+        <h1 class="text-2xl font-semibold mb-2">{{ $page.props.board.title || 'Untitled Board' }}</h1>
         <p class="text-primary-500">Description for {{ $page.props.board.id }} comes here...</p>
     </PublicSidebarLayout>
 </template>
