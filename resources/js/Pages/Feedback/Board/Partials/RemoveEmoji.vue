66e7efa8df5578355106e476bddc116385fb82c7
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-12-03 05:22:29 +0100

    removing emoji works

---
+++ resources/js/Pages/Feedback/Board/Partials/RemoveEmoji.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { router, usePage } from '@inertiajs/vue3'
+import { Button } from 'primevue';
+
+const page = usePage();
+const boardId = page.props.board.id;
+
+function onRemoveEmoji() {
+    router.patch(route('admin.board.emoji.update', boardId), { emoji: null })
+}
+</script>
+
+<template>
+    <div class="py-6 border-b border-gray-100">
+        <h2 class="text-xl font-semibold text-primary">
+            Remove Emoji
+        </h2>
+        <p class="mt-1 text-sm text-primary-600 mb-6">
+            You can remove {{ $page.props.board.emoji }} as the board emoji here
+        </p>
+        <Button
+            severity="secondary"
+            class="text-sm font-semibold"
+            @click="onRemoveEmoji"
+        >
+            Remove
+        </Button>
+    </div>
+</template>
--- resources/js/Pages/Feedback/Board/Settings/BoardSettings.vue
+++ resources/js/Pages/Feedback/Board/Settings/BoardSettings.vue
@@ -3,6 +3,7 @@ import { Head } from '@inertiajs/vue3';
 import BoardHeader from '../Partials/BoardHeader.vue';
 import AdminSidebarLayout from '@/Layouts/Sidebar/AdminSidebarLayout.vue';
 import DeleteBoard from '../Partials/DeleteBoard.vue';
+import RemoveEmoji from '../Partials/RemoveEmoji.vue';
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import DeleteBoard from '../Partials/DeleteBoard.vue';
     <AdminSidebarLayout>
         <template #header>Admin » Feedback » {{ $page.props.board.title || 'Untitled Board' }} » Settings</template>
         <BoardHeader :isAdminPage="true" />
+        <RemoveEmoji v-if="$page.props.board.emoji" />
         <DeleteBoard />
     </AdminSidebarLayout>
 </template>
