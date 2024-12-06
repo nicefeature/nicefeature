6f101bec946e81877ac7bf499d46e3aea06c5d95
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-12-06 23:13:53 +0100

    add select

--- resources/js/Pages/Feedback/Partials/NewFeedback.vue
+++ resources/js/Pages/Feedback/Partials/NewFeedback.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useForm } from '@inertiajs/vue3';
 import { PlusIcon } from 'lucide-vue-next';
-import { Button, Drawer, InputText, Textarea } from 'primevue';
+import { Button, Drawer, InputText, Select, Textarea } from 'primevue';
 import { nextTick, ref } from 'vue';
 
 const titleInputRef = ref<{ $el: HTMLElement } | null>(null);
@@ -14,10 +14,10 @@ function openDrawer() {
     })
 }
 
-// TODO: Add board id
 const form = useForm({
     title: '',
     description: '',
+    boardId: null,
 });
 
 const submit = () => {
@@ -39,7 +39,7 @@ const submit = () => {
         position="right"
         class="w-full md:w-1/2 p-8"
     >
-        <p class="text-primary-500 mb-12">Please submit your feedback here.</p>
+        <p class="text-primary-500 mb-12">Thank you for taking the time to give us feedback. Please fill out the form below.</p>
         <form @submit.prevent="submit">
             <div class="mb-8">
                 <label
@@ -72,6 +72,29 @@ const submit = () => {
                     rows="5"
                 />
             </div>
+            <div class="mb-8">
+                <label
+                    for="board"
+                    class="text-md font-semibold block mb-2"
+                >
+                    Category
+                </label>
+                <Select
+                    v-model="form.boardId"
+                    :options="$page.props.boards"
+                    optionLabel="name"
+                    placeholder="Select a board"
+                    checkmark :highlightOnSelect="false"
+                    class="w-full"
+                />
+            </div>
+            <Button
+                type="submit"
+                class="text-sm font-semibold"
+                :disabled="form.processing"
+            >
+                Submit
+            </Button>
         </form>
     </Drawer>
 </template>
